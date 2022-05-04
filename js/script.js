abe7bcdb71107664fca18c6d8b96ac2429d680c2
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-05-04 22:41:14 +0000

    7

--- js/script.js
+++ js/script.js
@@ -6,14 +6,30 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-03-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-03-HTML/",
   })
 }
+
+'use strict'
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function Verify() {
+  // input
+  const adult = document.getElementById("adult-age").checked
+  const teen = document.getElementById("teen-age").checked
+  const child = document.getElementById("child-age").checked
+
+  // process and output
+  if (adult == true) {
+    document.getElementById("check").innerHTML = 'You can see a R rated movie alone.'
+  } else if (teen == true) {
+    document.getElementById("check").innerHTML = 'You can see a PG-13 rated movie alone.'
+  } else if (child == true) {
+    document.getElementById("check").innerHTML = 'You can see a G or PG rated movie alone.'
+  } else {
+    document.getElementById("check").innerHTML = 'You are too young too watch a movie alone. Sorry!'
+  }
 }
